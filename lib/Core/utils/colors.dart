fa6fdcdfc34aad39cb50a30c48604afe8d168270
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2024-07-19 10:45:11 +0300

    Add Modification.

--- lib/Core/utils/colors.dart
+++ lib/Core/utils/colors.dart
@@ -5,6 +5,7 @@ abstract class AppColors {
   static const Color white = Colors.white;
   static const Color deepPurpleA700 = Color(0xFF4209CE);
   static const Color purple600 = Color(0xFF811AC3);
-  static const Color hintText = Color(0xFF9BA6A7);
+  static const Color grey = Color(0xFF9BA6A7);
   static const Color onBoardingTitleText = Color(0xFF6A6565);
+  static const Color smoothIndicator = Color(0xFF6B1ECD);
 }
